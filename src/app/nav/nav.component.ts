b40e2784377cb74904151745b044cd63895f5939
221hussein <yuzinpream51@gmail.com>
2023-11-22 03:26:51 +0000

    refactoring service for modal updating the components and correcting the bugs

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -13,9 +13,10 @@ export class NavComponent implements OnInit {
   ngOnInit(): void {
   
   }
+
   openModal ($event :Event) {
     $event.preventDefault
 
-    this.modal.toggleModal()
+    this.modal.toggleModal('auth')
   }
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 
 @Component({
@@ -10,6 +10,8 @@ import { ModalService } from '../../services/modal.service';
   // ]
 })
 export class ModalComponent implements OnInit {
+
+  @Input() modalID = ''
   constructor(public modal : ModalService) {
 
    }
@@ -18,7 +20,7 @@ export class ModalComponent implements OnInit {
     
   }
   closeModal() {
-    this.modal.toggleModal()
+    this.modal.toggleModal(this.modalID)
   }
 
 }
