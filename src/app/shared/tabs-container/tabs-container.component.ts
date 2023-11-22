0ef40caf1ef10d750f34554e9af8780207cb2120
221hussein <yuzinpream51@gmail.com>
2023-11-22 07:03:26 +0000

    Setting an ActiveTab

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -15,8 +15,25 @@ export class TabsContainerComponent implements AfterContentInit{
 
   constructor() { }
 
+  
   ngAfterContentInit(): void {
-   this.tabs?.first
+    // get all active tabs
+    const activeTabs = this.tabs?.filter(
+    tab => tab.active
+   )
+    // if there is no active tab set, activate the first
+   if (!activeTabs || activeTabs.length === 0) {
+    this.selectTab(this.tabs!.first)
+   }
+  }
+  selectTab(tab: TabComponent) {
+    //loop through all tabs
+    this.tabs?.forEach(tab => {
+      tab.active = false
+    }) 
+
+    tab.active = true
   }
 
+  
 }
