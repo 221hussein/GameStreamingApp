e39e3f51f94f7e30e1eb024cb116c28dfcd93427
221hussein <yuzinpream51@gmail.com>
2023-11-22 03:20:48 +0000

    refactoring service for modal and creating an id system (singleton)

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,21 +1,47 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
 
-  private visible = false
+  // private visible = false
 
-  constructor() {
-    
-   }
-   isModalOpen(){
-      return this.visible
+  private modals : IModal[] = [];
+
+
+  constructor() {}
+
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false
+    })
+  }
+
+   isModalOpen(id: string) : boolean {
+    // solution 1
+    // !! will return true if the value is not null or undefined
+      // return !!this.modals.find(element => element.id === id)?.visible
+
+      // solution 2
+      return Boolean(this.modals.find(element => element.id === id)?.visible)
     }
 
-    toggleModal(){
-      this.visible = !this.visible
+    toggleModal(id : string) {
+      const modal = this.modals.find(element => element.id === id)
+
+      if (modal) {
+        modal.visible = !modal.visible
+      }
+     // this.visible = !this.visible
     }
+
+
     
 }
